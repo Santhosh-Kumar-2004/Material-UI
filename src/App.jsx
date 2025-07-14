b57a45e59bda86ca24d73a1b2516c762d01227fb
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-07-14 12:11:15 +0530

    Typogrphy Component on MUI

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,24 @@
 import { AppBar, Toolbar, Typography, Box, Container } from '@mui/material'
 import ThemeToggle from './components/ThemeToggle'
 import BasicButtons from './samples/Buttons'
+import TypographyShowcase from './components/TypographyShowcase'
+import Divider from '@mui/material/Divider';
 
 function App() {
   return (
     <Box>
       <AppBar position="static" color="primary">
         <Toolbar>
-          <Typography variant="h6" sx={{ flexGrow: 1 }}>
-            MUI Theming Example
-          </Typography>
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>MUI Theming Example</Typography>
           <ThemeToggle />
         </Toolbar>
       </AppBar>
 
+      <Typography variant='h2' mt={5} mb={3}>Buttons Showcase</Typography>
       <BasicButtons />
+      <Divider sx={{ my: 3 }} />
+
+      <TypographyShowcase />
       
     </Box>
   )
--- src/samples/Buttons.jsx
+++ src/samples/Buttons.jsx
@@ -1,22 +1,47 @@
 import * as React from 'react';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
+import SendIcon from '@mui/icons-material/Send';
+import AlarmIcon from '@mui/icons-material/Alarm';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import IconButton from '@mui/material/IconButton';
 
 export default function BasicButtons() {
     return (
-        <Stack spacing={2} direction="row" mt={10}>
-            <Button variant="text">Text</Button>
-            <Button variant="contained">Contained</Button>
-            <Button variant="outlined">Outlined</Button>
-            <Button>Primary</Button>
-            <Button disabled>Disabled</Button>
-            <Button href="#text-buttons">Link</Button>
-            <Button variant="outlined">Primary</Button>
-            <Button variant="outlined" disabled>Disabled</Button>
-            <Button variant="outlined" href="#outlined-buttons">Link</Button>
-            <Button color="secondary">Secondary</Button>
-            <Button variant="contained" color="success">Success</Button>
-            <Button variant="outlined" color="error">Error</Button>
-        </Stack>
+        <>
+            <Stack spacing={2} direction="row">
+                <Button variant="text">Text</Button>
+                <Button variant="contained">Contained</Button>
+                <Button variant="outlined">Outlined</Button>
+                <Button>Primary</Button>
+                <Button disabled>Disabled</Button>
+                <Button href="#text-buttons">Link</Button>
+                <Button variant="outlined">Primary</Button>
+                <Button variant="outlined" disabled>Disabled</Button>
+                <Button variant="outlined" href="#outlined-buttons">Link</Button>
+                <Button color="secondary">Secondary</Button>
+                <Button variant="contained" color="success">Success</Button>
+                <Button variant="outlined" color="error">Error</Button>
+                <Button variant="outlined" startIcon={<DeleteIcon />}>Delete</Button>
+                <Button variant="contained" endIcon={<SendIcon />}>Send</Button>
+            </Stack>
+
+            <Stack spacing={2} direction="row">
+                <IconButton aria-label="delete">
+                    <DeleteIcon />
+                </IconButton>
+                <IconButton aria-label="delete" disabled color="primary">
+                    <DeleteIcon />
+                </IconButton>
+                <IconButton color="secondary" aria-label="add an alarm">
+                    <AlarmIcon />
+                </IconButton>
+                <IconButton color="primary" aria-label="add to shopping cart">
+                    <AddShoppingCartIcon />
+                </IconButton>
+            </Stack>
+
+        </>
     );
 }
